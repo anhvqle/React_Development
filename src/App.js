c7c4ea3ade145630d0f9e28165bba2bd58cfde96
Anh Le <a.v.le@wustl.edu>
2021-01-08 17:53:54 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "Anh Le";
+  const date = new Date();
+  let time = date.getHours() % 12;
+  let timeOfDay = "";
+  if(time < 12){
+    timeOfDay = "morning";
+  }
+  else if(time >= 12 && time < 18){
+    timeOfDay = "afternoon";
+  }
+  else{
+    timeOfDay = "night";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Good {timeOfDay}, {name}!</h1>
       </header>
     </div>
   );
