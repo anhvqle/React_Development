0c3b52389df6cb6b755a97ebe44709dd555ff042
Anh Le <a.v.le@wustl.edu>
2021-01-11 14:10:16 +0700

    client login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
 
 function App() {
-  let name = "Anh Le";
-  const date = new Date();
-  let time = date.getHours() % 12;
-  let timeOfDay = "";
-  if(time < 12){
-    timeOfDay = "morning";
-  }
-  else if(time >= 12 && time < 18){
-    timeOfDay = "afternoon";
-  }
-  else{
-    timeOfDay = "night";
-  }
   return (
-    <div className="App">
-      <header>
-        <h1>Good {timeOfDay}, {name}!</h1>
-      </header>
+    <div className="jumbotron">
+      <p><label>Username: <input type="text" name="username" id="username" required></input></label></p>
+      <p><label>Password: <input type="password" name="password" id="password" required></input></label></p>
+      <p><input class="btn btn-primary" type="submit" name="login" value="Log In" id="login_btn"></input></p>
+      <p id="login_message"></p>
+      <p id="logout"><input class="btn btn-primary" type="submit" style={{display:"none"}} name="logout" value="Logout" id="logout_btn"></input></p>
     </div>
   );
 }
---
+++ client/src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav class="navbar navbar-default navfont navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <ul class="nav navbar-nav"><li><a class="navbar-link" href="/login">Log In</a></li></ul>
+                    <ul class="nav navbar-nav"><li><a class="navbar-link" href="/register">Register</a></li></ul>
+                </div>
+            </div>
+        </nav> 
+    );
+  }
+  
+  export default Navbar;
