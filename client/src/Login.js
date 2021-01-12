b3020094c1885cbbde01543f2c7c90765ec42100
Anh Le <a.v.le@wustl.edu>
2021-01-12 17:44:22 +0700

    Reload functionality done

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,39 +5,57 @@ let token = undefined;
 let msg = "";
 
 function Login() {
-    const [loginUsername, setLoginUsername] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
+    document.addEventListener("DOMContentLoaded", function(event) {
+        console.log("Refresh button pressed");
+        Axios.post("http://localhost:3001/user/checkToken", {
+            token: localStorage.getItem('token')
+        }).then( (response) => {
+            if(parseInt(response.data.status) <= 299){
+                token = response.data.token;
+                isLogin(true);
+                msg = "Login Succesfully";
+            }
+            else{
+                msg = "Incorrect username or password!";
+                console.log(msg);
+            }
+            setLoginStatus(response.data.message);
+        }).catch((error) => {
+            console.log(error);
+        })
+    });
 
     let [loginStatus, setLoginStatus] = useState("");
     let [isLoggedIn, isLogin] = useState(Boolean);
 
+    const [loginUsername, setLoginUsername] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
     const login = () => {
-    console.log("log in pressed");
+        console.log("log in pressed");
         Axios.post("http://localhost:3001/user/login", {
-        username: loginUsername,
-        password: loginPassword
+            username: loginUsername,
+            password: loginPassword
         }).then( (response) => {
-        console.log(response.data);
-        if(parseInt(response.data.status) <= 299){
-            token = response.data.token;
-            isLogin(true);
-            localStorage.setItem('token', token);
-            msg = "Login Succesfully";
-        }
-        else{
-            msg = "Incorrect username or password!";
-            console.log(msg);
-        }
-        setLoginStatus(response.data.message);
+            if(parseInt(response.data.status) <= 299){
+                token = response.data.token;
+                isLogin(true);
+                localStorage.setItem('token', token);
+                msg = "Login Succesfully";
+            }
+            else{
+                msg = "Incorrect username or password!";
+                console.log(msg);
+            }
+            setLoginStatus(response.data.message);
         }).catch((error) => {
-        console.log(error);
+            console.log(error);
         })
     };
 
     const logout = () => {
         console.log("log out pressed");
         Axios.get("http://localhost:3001/user/logout").then( (response) => {
-            console.log(response.data);
             if(parseInt(response.data.status) <= 299){
                 localStorage.removeItem('token');
                 isLogin(false);
@@ -47,28 +65,31 @@ function Login() {
                 msg = "Logout Unsuccesfully";
             }
             setLoginStatus(response.data.message);
-            }).catch((error) => {
+        }).catch((error) => {
             console.log(error);
         })
     };
 
     return (
         <div className="jumbotron">
-            <p>
-            <label>
-                Username: <input type="text" name="username" id="username" required onChange = {(e) => {
-                setLoginUsername(e.target.value);
-                }}></input>
-                </label>
-            </p>
-            <p>
-            <label>
-                Password: <input type="password" name="password" id="password" required onChange = {(e) => {
-                setLoginPassword(e.target.value);
-                }}></input>
-                </label>
-            </p>
-            <p><button className="btn btn-primary" type="submit" style={{display: isLoggedIn ? "none" : "block"}} name="login" id="login_btn" onClick = {login}>Log In</button></p>
+            <h1 style={{display: isLoggedIn ? "block" : "none"}} >Hello World</h1>
+            <div style={{display: isLoggedIn ? "none" : "block"}}>
+                <p>
+                <label>
+                    Username: <input type="text" name="username" id="username" required onChange = {(e) => {
+                    setLoginUsername(e.target.value);
+                    }}></input>
+                    </label>
+                </p>
+                <p>
+                <label>
+                    Password: <input type="password" name="password" id="password" required onChange = {(e) => {
+                    setLoginPassword(e.target.value);
+                    }}></input>
+                    </label>
+                </p>
+                <p><button className="btn btn-primary" type="submit" name="login" id="login_btn" onClick = {login}>Log In</button></p>
+            </div>
             <p>{loginStatus}</p>
             <p><button className="btn btn-primary" type="submit" style={{display: isLoggedIn ? "block" : "none"}} name="logout" id="logout_btn" onClick = {logout}>Log Out</button></p>
         </div>
--- client/src/Register.js
+++ client/src/Register.js
@@ -4,65 +4,65 @@ import { useState } from 'react';
 let msg = "";
 
 function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [fullname, setFullname] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [fullname, setFullname] = useState("");
 
-  let [registerStatus, setRegisterStatus] = useState("");
+    let [registerStatus, setRegisterStatus] = useState("");
 
-  const register = () => {
-    console.log("log in pressed");
-    Axios.post("http://localhost:3001/user/register", {
-      username: username,
-      password: password,
-      email: email,
-      fullname: fullname
-    }).then( (response) => {
-      console.log(response.data);
-      if(parseInt(response.data.status) <= 299){
-        msg = "Register Succesfully";
-      }
-      else{
-        msg = "Register Unsuccesfully - " + response.data.message;
-      }
-      setRegisterStatus(msg);
-    }).catch((error) => {
-      console.log(error);
-    })
-  };
-  return (
-    <div className="jumbotron">
-        <p>
-            <label>Full Name: <input type="text" name="fullname" id="register_fullname" required onChange = {(e) => {
-                    setFullname(e.target.value);
-                }}></input>
-            </label>
-        </p>
-        <p>
-            <label>Email: <input type="email" name="email" id="register_email" required onChange = {(e) => {
-                    setEmail(e.target.value);
-                }}></input>
-            </label>
-        </p>
-        <p>
-            <label>
-                Username: <input type="text" name="username" id="username" required onChange = {(e) => {
-                    setUsername(e.target.value);
-                }}></input>
-            </label>
-        </p>
-        <p>
-            <label>
-                Password: <input type="password" name="password" id="password" required onChange = {(e) => {
-                    setPassword(e.target.value);
-                }}></input>
-            </label>
-        </p>
-        <p><button className="btn btn-primary" type="submit" name="register" id="register_btn" onClick = {register}>Register</button></p>
-        <p>{registerStatus}</p>
-    </div>
-  );
+    const register = () => {
+        console.log("log in pressed");
+        Axios.post("http://localhost:3001/user/register", {
+            username: username,
+            password: password,
+            email: email,
+            fullname: fullname
+        }).then( (response) => {
+            console.log(response.data);
+            if(parseInt(response.data.status) <= 299){
+                msg = "Register Succesfully";
+            }
+            else{
+                msg = "Register Unsuccesfully - " + response.data.message;
+            }
+            setRegisterStatus(msg);
+        }).catch((error) => {
+            console.log(error);
+        })
+    };
+    return (
+        <div className="jumbotron">
+            <p>
+                <label>Full Name: <input type="text" name="fullname" id="register_fullname" required onChange = {(e) => {
+                        setFullname(e.target.value);
+                    }}></input>
+                </label>
+            </p>
+            <p>
+                <label>Email: <input type="email" name="email" id="register_email" required onChange = {(e) => {
+                        setEmail(e.target.value);
+                    }}></input>
+                </label>
+            </p>
+            <p>
+                <label>
+                    Username: <input type="text" name="username" id="username" required onChange = {(e) => {
+                        setUsername(e.target.value);
+                    }}></input>
+                </label>
+            </p>
+            <p>
+                <label>
+                    Password: <input type="password" name="password" id="password" required onChange = {(e) => {
+                        setPassword(e.target.value);
+                    }}></input>
+                </label>
+            </p>
+            <p><button className="btn btn-primary" type="submit" name="register" id="register_btn" onClick = {register}>Register</button></p>
+            <p>{registerStatus}</p>
+        </div>
+    );
 }
 
 export default Register;
